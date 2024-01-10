d7d99d3f714e7e936150ed207e2cd1ed303d3c98
opicallo <83368250+opicallo@users.noreply.github.com>
2024-01-10 14:15:33 +0100

    Update stage.sql

--- stage.sql
+++ stage.sql
@@ -1,11 +1,20 @@
-create or replace TABLE NIMBUS_DB.PUBLIC.USERS (
-    result varchar
+CREATE OR REPLACE STAGE nimbus
+  URL='s3://frostyfridaychallenges/challenge_1/'
+
+  list @nimbus
+
+
+create or replace TABLE nimbus_table (
+ result varchar
 );
 
-copy into NIMBUS_DB.PUBLIC.USERS
+copy into nimbus_table
     from s3://frostyfridaychallenges/challenge_1/
     file_format = (
         type = csv
         field_delimiter = ','
         skip_header = 1
     );
+
+select * 
+from nimbus_table
