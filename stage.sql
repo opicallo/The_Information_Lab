be4b8141eabe729ed92a3d98e276166c7baf9ace
opicallo <83368250+opicallo@users.noreply.github.com>
2024-01-10 14:04:58 +0100

    Update stage.sql

--- stage.sql
+++ stage.sql
@@ -1,8 +1,8 @@
-create or replace TABLE EXAMPLE_DB.PUBLIC.USERS (
+create or replace TABLE NIMBUS_DB.PUBLIC.USERS (
     result varchar
 );
 
-copy into EXAMPLE_DB.PUBLIC.USERS
+copy into NIMBUS_DB.PUBLIC.USERS
     from s3://frostyfridaychallenges/challenge_1/
     file_format = (
         type = csv
